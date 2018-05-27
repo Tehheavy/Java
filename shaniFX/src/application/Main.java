a113fe40a8ddba982d02de51eb55071858e82969
shani <shani@DESKTOP-49862SU.mynet>
2018-05-27 18:41:22 +0300

    first

---
+++ shaniFX/src/application/Main.java
@@ -0,0 +1,23 @@
+package application; 
+ 
+import java.io.IOException; import java.net.URL; 
+ 
+import javafx.application.Application; import javafx.fxml.FXMLLoader; import javafx.scene.Scene; import javafx.scene.layout.AnchorPane; import javafx.stage.Stage; 
+ 
+public class Main extends Application { 
+ 
+  @Override   public void start(Stage primaryStage) throws IOException {
+	  // constructing our scene    
+	  URL url = getClass().getResource("hello.fxml"); 
+	  AnchorPane pane = FXMLLoader.load( url ); 
+	  Scene scene = new Scene( pane );       
+	  // setting the stage  
+	  primaryStage.setScene( scene );  
+	  primaryStage.setTitle( "Hello World Demo-313313983;313207888;313313991;328737739" );   
+	  primaryStage.show();     
+	  }
+  public static void main(String[] args)
+  { 
+	  launch(args); 
+	  } } 
+  
